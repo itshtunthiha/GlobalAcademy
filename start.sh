d55803482fe81a8e6853ad84ad66debc78019665
Konyi <itshtunthiha@gmail.com>
2025-11-20 17:25:14 +0700

    emptied start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Navigate into your webservice folder if your pom.xml is there
-cd webservice
+# cd webservice
 
 # Build the app using Maven (if you use Gradle, adjust accordingly)
-bash mvnw clean package
+# bash mvnw clean package
 
 # Run the Spark server
 # java -cp target/spark-hello-1.0-SNAPSHOT-jar-with-dependencies.jar org.global.academy.Server
-java -jar target/spark-hello-1.0-SNAPSHOT-jar-with-dependencies.jar
+# java -jar target/spark-hello-1.0-SNAPSHOT-jar-with-dependencies.jar
 
