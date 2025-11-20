45af07d134bceceeb4f249fb5f5cffdd03e4b7d8
Konyi <itshtunthiha@gmail.com>
2025-11-21 02:20:27 +0700

    made the web load faster

--- webservice/src/main/java/org/global/academy/Server.java
+++ webservice/src/main/java/org/global/academy/Server.java
@@ -2,6 +2,7 @@ package org.global.academy;
 
 import com.google.gson.Gson;
 
+import static spark.Spark.after;
 import static spark.Spark.before;
 import static spark.Spark.get;
 import static spark.Spark.halt;
@@ -9,11 +10,21 @@ import static spark.Spark.options;
 import static spark.Spark.port;
 import static spark.Spark.post;
 import static spark.Spark.staticFiles;
+import static spark.Spark.threadPool;
 
 public class Server {
     public static void main(String[] args) {
         port(8080);
+
+        // ⚡ Faster server thread pool
+        threadPool(40, 5, 30000);
+
+        // ⚡ Cache static files for 30 days
         staticFiles.location("/public");
+        staticFiles.expireTime(3600 * 24 * 30);
+
+        // Allow GZIP
+        after((req, res) -> res.header("Content-Encoding", "gzip"));
 
         // Simple CORS
         before((request, response) -> {
@@ -25,17 +36,18 @@ public class Server {
         options("/*", (req, res) -> {
             String headers = req.headers("Access-Control-Request-Headers");
             if (headers != null) res.header("Access-Control-Allow-Headers", headers);
+
             String method = req.headers("Access-Control-Request-Method");
             if (method != null) res.header("Access-Control-Allow-Methods", method);
+
             return "OK";
         });
 
         Gson gson = new Gson();
 
-        // 🔒 LOGIN REQUIRED FOR FLASHCARD ROUTES
+        // Authentication
         before("/flashcards/*", (req, res) -> {
             String auth = req.headers("Authorization");
-
             if (auth == null || !auth.equals("Bearer a-fake-token")) {
                 res.status(401);
                 res.type("application/json");
@@ -46,16 +58,14 @@ public class Server {
         get("/flashcards/default", (req, res) -> {
             res.type("application/json");
             return gson.toJson(UseCard.getAllCards());
-        });        
+        });
 
-        // Protected API — must be logged in
         get("/flashcards/random", (req, res) -> {
             Flashcard card = UseCard.getRandomCard();
             res.type("application/json");
             return gson.toJson(card);
         });
 
-        // Public — Login endpoint
         post("/login", (req, res) -> {
             System.out.println("Received /login request with body: " + req.body());
             LoginRequest lr = gson.fromJson(req.body(), LoginRequest.class);
@@ -89,8 +99,6 @@ public class Server {
     static class ErrorResponse {
         String error;
 
-        ErrorResponse(String e) {
-            error = e;
-        }
+        ErrorResponse(String e) { error = e; }
     }
 }
